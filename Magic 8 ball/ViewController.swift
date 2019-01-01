8e1efd513359053db0264921a81febda3c9b1b1b
sic <user@macOS-Dev.local>
2019-01-01 12:22:58 -0600

    first check in

---
+++ Magic 8 ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by ADT on 1/1/19.
+//  Copyright © 2019 ADT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
