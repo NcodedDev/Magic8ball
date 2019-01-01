2f8cc854704158026e09ebe900da72440f5064bf
sic <user@macOS-Dev.local>
2019-01-01 12:54:18 -0600

    project working

--- Magic 8 ball/ViewController.swift
+++ Magic 8 ball/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ivBall: UIImageView!
+    @IBOutlet var mainVC: UIView!
+    
+    let arrBallNames : Array = ["ball1","ball2","ball4","ball4","ball5"]
+    var iRandomNumber : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadRandomImage()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btnAsk_Click(_ sender: Any) {
+        loadRandomImage()
+    }
+    
+    func loadRandomImage (){
 
+        iRandomNumber = Int.random(in: 0...4)
+        ivBall.image = UIImage(named: arrBallNames[iRandomNumber])
+        print(arrBallNames[iRandomNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+
+        loadRandomImage()
+    }
 }
 
